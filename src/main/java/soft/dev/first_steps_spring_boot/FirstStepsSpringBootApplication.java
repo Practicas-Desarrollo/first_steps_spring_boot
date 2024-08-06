968d996d1303daecfe2b59b9acd8159803b95812
José Luis Terán <joseluisteran.study@gmail.com>
2024-08-06 19:04:52 -0400

    build: set up Java project using Maven

---
+++ src/main/java/soft/dev/first_steps_spring_boot/FirstStepsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package soft.dev.first_steps_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStepsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soft/dev/first_steps_spring_boot/FirstStepsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.dev.first_steps_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
