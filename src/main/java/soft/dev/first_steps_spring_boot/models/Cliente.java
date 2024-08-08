3bddf9f7ad2bd0d714a98931ff7f5a4bb9c56bff
José Luis Terán <joseluisteran.study@gmail.com>
2024-08-08 08:37:35 -0400

    feat: modelos de personas terminado

--- src/main/java/soft/dev/first_steps_spring_boot/models/Cliente.java
+++ src/main/java/soft/dev/first_steps_spring_boot/models/Cliente.java
@@ -1,4 +1,42 @@
 package soft.dev.first_steps_spring_boot.models;
 
-public class Cliente {
+import java.util.UUID;
+
+public class Cliente extends Persona {
+  private UUID codigoCliente;
+  private int ci;
+  private int nit;
+
+  public Cliente(UUID id, String nombre, String apellidoPaterno,
+                 String apellidoMaterno, String genero, Rol tipo,
+                 UUID codigoCliente, int ci, int nit) {
+    super(id, nombre, apellidoPaterno, apellidoMaterno, genero, tipo);
+    this.codigoCliente = codigoCliente;
+    this.ci = ci;
+    this.nit = nit;
+  }
+
+  public UUID getCodigoCliente() {
+    return codigoCliente;
+  }
+
+  public void setCodigoCliente(UUID codigoCliente) {
+    this.codigoCliente = codigoCliente;
+  }
+
+  public int getCi() {
+    return ci;
+  }
+
+  public void setCi(int ci) {
+    this.ci = ci;
+  }
+
+  public int getNit() {
+    return nit;
+  }
+
+  public void setNit(int nit) {
+    this.nit = nit;
+  }
 }
--- src/main/java/soft/dev/first_steps_spring_boot/models/Rol.java
+++ src/main/java/soft/dev/first_steps_spring_boot/models/Rol.java
@@ -1,4 +1,39 @@
 package soft.dev.first_steps_spring_boot.models;
 
+import java.util.UUID;
+
 public class Rol {
+  private UUID id;
+  private String nombre;
+  private int estado;
+
+  public Rol(UUID id, String nombre, int estado) {
+    this.id = id;
+    this.nombre = nombre;
+    this.estado = estado;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public int getEstado() {
+    return estado;
+  }
+
+  public void setEstado(int estado) {
+    this.estado = estado;
+  }
 }
